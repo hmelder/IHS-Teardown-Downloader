bde317be006114283e6de9b4da849791d893556e
Hugo Melder <45601940+DerNuntius@users.noreply.github.com>
2020-07-24 22:27:59 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
 	name = "ihs-dl",
-	version = "0.0.1",
+	version = "0.0.2",
 	license = "GPLv3",
 	description = "IHS Markit Teardown Photo Downloader",
 	author = "DerNuntius",
